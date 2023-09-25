5a0663b7c43ee9fcae0430d886d974a2ea10a358
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-09-25 15:13:49 +0200

    created special file

---
+++ src/components/ex-1/people.js
@@ -0,0 +1,26 @@
+import {
+    people01,
+    people02,
+    people03,
+} from "../../assets/ex-1/images/index";
+
+
+const people = [
+    {
+        name: 'Elli',
+        message: 'Test message',
+        photo: people01
+    },
+    {
+        name: 'Leah',
+        message: 'Test message2',
+        photo: people02
+    },
+    {
+        name: 'Samantha',
+        message: `Hi there! How can I help you?`,
+        photo: people03
+    },
+];
+
+export default people
