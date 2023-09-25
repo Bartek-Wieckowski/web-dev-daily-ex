17e63abbd04117886973bb962b9a358962d72eb6
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-09-25 15:56:56 +0200

    added msg icon

--- src/assets/ex-1/images/index.js
+++ src/assets/ex-1/images/index.js
@@ -1,9 +1,11 @@
 import people01 from "./1.jpg";
 import people02 from "./2.jpg";
 import people03 from "./3.jpg";
+import msgIcon from "./envelope.svg"
 
 export {
     people01,
     people02,
     people03,
+    msgIcon
 };
