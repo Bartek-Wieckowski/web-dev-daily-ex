7647b44d10250446e0ab535620a641042c980716
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-09-26 16:10:36 +0200

    added two necessarry pages

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,12 @@
+const Home = () => {
+  return (
+    <div>
+      <h1 className="txt-md md:text-4xl text-center">Welcome!</h1>
+      <h3 className="txt-sm md:text-xl text-center">
+        Click next/prev to visible my Daily Web Dec Coding challange with React
+      </h3>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/page-not-found/ex-2/PageNotFound.jsx
@@ -0,0 +1,5 @@
+const PageNotFound = () => {
+  return <div>Sorry</div>;
+};
+
+export default PageNotFound;
