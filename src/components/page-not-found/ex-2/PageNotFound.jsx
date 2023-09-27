29a69f05acc4e10e2a664d6d7177e84c9e7b7ecc
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-09-27 11:52:36 +0200

    finished ex 2 and also used this component to display 404 ;))

--- src/components/page-not-found/ex-2/PageNotFound.jsx
+++ src/components/page-not-found/ex-2/PageNotFound.jsx
@@ -1,5 +1,24 @@
+import { Link } from 'react-router-dom';
+
 const PageNotFound = () => {
-  return <div>Sorry</div>;
+  return (
+    <section className="text-white bg-gradient-to-r from-purple-200 to-pink-500 ">
+      <div className="flex flex-col items-center justify-center gap-10 h-[calc(100vh-56px)]">
+        <h1 className="text-[120px] leading-none">404</h1>
+        <h2 className="text-5xl">Page Not Found</h2>
+        <div className="text-center">
+          <p>The page you are looking for doesn`t exist or an other</p>
+          <p>
+            Error occurred. Go to
+            <Link to="/">
+              {' '}
+              <span className="text-indigo-600 underline">Home</span>
+            </Link>
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default PageNotFound;
