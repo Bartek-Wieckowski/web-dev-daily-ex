634996f7196852f39cd9701d5ac9819edf7af62e
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-09-27 11:53:05 +0200

    changed place BrowserRouter and use useLocation hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useState, Suspense, lazy } from 'react';
-import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
+import { useState, Suspense, lazy, useEffect } from 'react';
+import { Route, Routes, Link, useLocation } from 'react-router-dom';
 // components/pages
 const Home = lazy(() => import('./components/home/Home'));
 const PageNotFound = lazy(() =>
@@ -11,6 +11,11 @@ import Spinner from './components/spinner/Spinner';
 
 function App() {
   const [showExNum, setShowExNum] = useState(0);
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.pathname === '/') setShowExNum(0);
+  }, [location.pathname]);
 
   const handleClickPrev = () => {
     if (showExNum > 1) {
@@ -25,34 +30,32 @@ function App() {
 
   return (
     <section>
-      <BrowserRouter>
-        <Suspense fallback={<Spinner />}>
-          <nav className="container px-6 mx-auto nav">
-            <ul className="flex items-center justify-between p-4">
-              <Link
-                to={showExNum > 1 ? `ex${showExNum - 1}` : '/'}
-                className={`${
-                  showExNum === 0
-                    ? 'opacity-50 cursor-not-allowed pointer-events-none'
-                    : ''
-                }`}
-              >
-                <li onClick={() => handleClickPrev()}>prev</li>
-              </Link>
-              <Link to={`ex${showExNum + 1}`}>
-                <li onClick={() => handleClickNext()}>next</li>
-              </Link>
-            </ul>
-          </nav>
+      <Suspense fallback={<Spinner />}>
+        <nav className="container px-6 mx-auto nav">
+          <ul className="flex items-center justify-between p-4">
+            <Link
+              to={showExNum > 1 ? `ex${showExNum - 1}` : '/'}
+              className={`${
+                showExNum === 0
+                  ? 'opacity-50 cursor-not-allowed pointer-events-none'
+                  : ''
+              }`}
+            >
+              <li onClick={() => handleClickPrev()}>prev</li>
+            </Link>
+            <Link to={`ex${showExNum + 1}`}>
+              <li onClick={() => handleClickNext()}>next</li>
+            </Link>
+          </ul>
+        </nav>
 
-          <Routes>
-            <Route path="/" exact element={<Home />} />
-            <Route path="/ex1" element={<Exercise1 />} />
-            <Route path="/ex2" element={<PageNotFound />} />
-            <Route path="*" element={<PageNotFound />} />
-          </Routes>
-        </Suspense>
-      </BrowserRouter>
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/ex1" element={<Exercise1 />} />
+          <Route path="/ex2" element={<PageNotFound />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </Suspense>
     </section>
   );
 }
