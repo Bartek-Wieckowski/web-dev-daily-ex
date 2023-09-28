a9de9b966d3ea75adad149fa9cd3cb8fa98c923d
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-09-28 16:10:17 +0200

    used Button component, created useState, functions and added logic to basic effect collapse

--- src/components/ex-4/Wrapper.jsx
+++ src/components/ex-4/Wrapper.jsx
@@ -1,15 +1,32 @@
+import { useState } from 'react';
+import Button from './Button';
+
 const Wrapper = ({ children }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  function handleClick() {
+    setIsExpanded((prev) => !prev);
+  }
+
   return (
-    <div className="container mx-auto">
-      <div className="w-[90%] bg-white rounded-3xl mx-auto p-8">
+    <div className="container mx-auto p-3">
+      <div className="w-[90%] bg-white rounded-3xl mx-auto p-8 overflow-hidden relative">
         <h3 className="font-medium text-center text-lg">Recently added</h3>
         <p className="font-normal text-center text-sm mt-3">
           The latest tools that help you and your team build software
           better,together.
         </p>
-        <div className="mt-12 grid grid-cols-1 md:grid-cols-2 gap-3 md:gap-6  xl:gap-12">
+        <div
+          className={`mt-12 grid grid-cols-1 md:grid-cols-2 gap-3 md:gap-6  xl:gap-12 ${
+            isExpanded
+              ? 'ex4-expand-transition'
+              : 'max-h-[215px] overflow-hidden'
+          }`}
+        >
           {children}
         </div>
+        <div className={`${!isExpanded ? 'ex4-blur' : ''}`}></div>
+        <Button onExpanded={handleClick} />
       </div>
     </div>
   );
