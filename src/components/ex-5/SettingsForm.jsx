77bbf39dfa0cfcb190f76abca7584e57739dc136
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-09-29 17:47:00 +0200

    created child-parent data flow

--- src/components/ex-5/SettingsForm.jsx
+++ src/components/ex-5/SettingsForm.jsx
@@ -1,4 +1,13 @@
-const SettingsForm = () => {
+import { useState } from 'react';
+
+const SettingsForm = ({ onSettingsUserSubmit }) => {
+  const [date, setDate] = useState('');
+  const [time, setTime] = useState('');
+
+  const handleSubmit = () => {
+    onSettingsUserSubmit([date, time]);
+  };
+
   return (
     <div className="bg-transparent max-w-md rounded-3xl p-6 fixed top-40  right-10 border border-1">
       <form>
@@ -14,6 +23,7 @@ const SettingsForm = () => {
             id="date"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             required=""
+            onChange={(e) => setDate(e.target.value)}
           />
         </div>
         <div className="mb-6">
@@ -28,10 +38,12 @@ const SettingsForm = () => {
             id="time"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             required=""
+            onChange={(e) => setTime(e.target.value)}
           />
         </div>
         <div className="mb-6">
           <button
+            onClick={() => handleSubmit()}
             type="button"
             className="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
           >
