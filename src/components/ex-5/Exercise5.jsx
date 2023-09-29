720846e6d69ce3707c4590296b88feb6ae9d2664
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-09-29 17:47:34 +0200

    added useState and change func names

--- src/components/ex-5/Exercise5.jsx
+++ src/components/ex-5/Exercise5.jsx
@@ -1,10 +1,17 @@
 import { useState } from 'react';
 import { gear } from '../../assets/ex-5';
 import SettingsForm from './SettingsForm';
+
 const Exercise5 = () => {
   const [isOpenSettings, setIsOpenSettings] = useState(false);
-  const handleClickOpen = () => {
+  const [userChoice, setUserChoice] = useState([]);
+
+  const handleToggleSettings = () => {
     setIsOpenSettings((prev) => !prev);
+  };
+
+  const onSettingsUserSubmit = (value) => {
+    setUserChoice(value);
   };
 
   return (
@@ -12,11 +19,16 @@ const Exercise5 = () => {
       <div className="bg-[url('./assets/ex-5/bgex5.jpg')] bg-cover bg-center w-full no_scroll relative">
         <button
           className="absolute right-10 top-10 w-[45px] h-[45px] cursor-pointer"
-          onClick={() => handleClickOpen()}
+          onClick={() => handleToggleSettings()}
         >
           <img src={gear} alt="" />
         </button>
-        {isOpenSettings && <SettingsForm isOpen={isOpenSettings} />}
+        {isOpenSettings && (
+          <SettingsForm
+            isOpen={isOpenSettings}
+            onSettingsUserSubmit={onSettingsUserSubmit}
+          />
+        )}
         <div className="flex items-center justify-center max-w-[800px] mx-auto  h-1/2"></div>
       </div>
     </section>
