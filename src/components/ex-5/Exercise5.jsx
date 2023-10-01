efa51806ec566c6bdad83e8dcea03673f487ca1a
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-10-01 13:49:25 +0200

    users data in object passed to props

--- src/components/ex-5/Exercise5.jsx
+++ src/components/ex-5/Exercise5.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { gear } from '../../assets/ex-5';
 import SettingsForm from './SettingsForm';
+import CountdownTimer from './CountdownTimer';
 
 const Exercise5 = () => {
   const [isOpenSettings, setIsOpenSettings] = useState(false);
@@ -8,8 +9,17 @@ const Exercise5 = () => {
 
   const splitDate = userChoice[0] ? userChoice[0].split('-') : [];
   const splitTime = userChoice[1] ? userChoice[1].split(':') : [];
+  const [year, month, day] = splitDate;
+  const [hours, minutes] = splitTime;
 
-  const userValues = [...splitDate, ...splitTime];
+  const userValuesObj = {
+    year: Number(year),
+    month: Number(month),
+    days: Number(day),
+    hours: Number(hours),
+    minutes: Number(minutes),
+    seconds: 0,
+  };
 
   const handleToggleSettings = () => {
     setIsOpenSettings((prev) => !prev);
@@ -19,7 +29,6 @@ const Exercise5 = () => {
     setUserChoice(value);
   };
 
-  console.log(userValues);
   return (
     <section className="bg-blue-100 text-black flex flex-col justify-center ">
       <div className="bg-[url('./assets/ex-5/bgex5.jpg')] bg-cover bg-center w-full no_scroll relative">
@@ -35,10 +44,13 @@ const Exercise5 = () => {
             onSettingsUserSubmit={onSettingsUserSubmit}
           />
         )}
-        <div className="flex items-center justify-center max-w-[800px] mx-auto  h-1/2 no_scroll">
+        <div className="flex flex-col items-center justify-center max-w-[800px] mx-auto  h-1/2 no_scroll gap-y-4">
           <h1 className="text-5xl text-gray-300 font-medium leading-normal">
             Until new beginnings
           </h1>
+          <div>
+            <CountdownTimer targetDate={userValuesObj} />
+          </div>
         </div>
       </div>
     </section>
