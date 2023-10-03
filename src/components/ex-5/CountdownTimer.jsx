fee1b5c14cff6279890c690ee6c9917123ebafaa
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-10-03 11:36:56 +0200

    formatted values, and add logic to not show NaN values

--- src/components/ex-5/CountdownTimer.jsx
+++ src/components/ex-5/CountdownTimer.jsx
@@ -6,19 +6,11 @@ const CountdownTimer = ({ targetDate }) => {
 
   const [pulseAlert, setPulseAlert] = useState(false);
 
-  const formattedHours = hours === 0 ? '0' : hours?.toString().padStart(2, '0');
+  const formattedHours = hours === 0 ? '0' : hours.toString().padStart(2, '0');
   const formattedMinutes =
-    minutes === 0 ? '0' : minutes?.toString().padStart(2, '0');
+    minutes === 0 ? '0' : minutes.toString().padStart(2, '0');
   const formattedSeconds =
-    seconds === 0 ? '0' : seconds?.toString().padStart(2, '0');
-
-  //   const countdownExpired = days + hours + minutes + seconds <= 0;
-
-  //   useEffect(() => {
-  //     if (countdownExpired) {
-  //       onCountdownExpired();
-  //     }
-  //   }, [countdownExpired, onCountdownExpired]);
+    seconds === 0 ? '0' : seconds.toString().padStart(2, '0');
 
   useEffect(() => {
     const remainingTimeInSeconds =
@@ -35,19 +27,25 @@ const CountdownTimer = ({ targetDate }) => {
       }`}
     >
       <div className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:text-white">
-        <p className="text-5xl">{days}</p>
+        <p className="text-5xl">{!isNaN(days) ? days : ''}</p>
         <p className="txt-sm">Days</p>
       </div>
       <div className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:text-white">
-        <p className="text-5xl">{formattedHours}</p>
+        <p className="text-5xl">
+          {!isNaN(formattedHours) ? formattedHours : ''}
+        </p>
         <p className="txt-sm">Hours</p>
       </div>
       <div className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:text-white">
-        <p className="text-5xl">{formattedMinutes}</p>
+        <p className="text-5xl">
+          {!isNaN(formattedMinutes) ? formattedMinutes : ''}
+        </p>
         <p className="txt-sm">Minutes</p>
       </div>
       <div className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:text-white">
-        <p className="text-5xl">{formattedSeconds}</p>
+        <p className="text-5xl">
+          {formattedSeconds >= 0 ? formattedSeconds : ''}
+        </p>
         <p className="txt-sm">Seconds</p>
       </div>
     </div>
