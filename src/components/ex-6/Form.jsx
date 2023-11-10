3e55fa9effcefbca6c00636effc5318c61cdd430
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-11-10 20:50:07 +0100

    created login tabs inside form

--- src/components/ex-6/Form.jsx
+++ src/components/ex-6/Form.jsx
@@ -13,7 +13,7 @@ const Form = () => {
   };
 
   return (
-    <div className="relative max-w-[500px] min-h-[25vh] mx-auto bg-slate-700 text-white">
+    <div className="relative max-w-[500px] pt-10 pb-20 mx-auto bg-slate-700 text-white rounded-3xl">
       <div className="absolute -top-7 left-1/2 -translate-x-1/2 w-14 h-14 bg-teal-400"></div>
       <div className="flex items-center justify-between max-w-[300px] py-10 mx-auto">
         <button
@@ -34,15 +34,46 @@ const Form = () => {
         </button>
       </div>
 
-      <form>
-        <input
-          type="search"
-          id="default-search"
-          className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder="Search Mockups, Logos..."
-          required
-        ></input>
-      </form>
+      {activeTabLogIn && (
+        <form>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            className="block w-[80%] mx-auto px-4 py-3 ps-10 text-sm rounded-2xl bg-gray-600 text-white "
+            placeholder="Email"
+            required
+          ></input>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            className="block w-[80%] mx-auto mt-6 px-4 py-3 ps-10 text-sm rounded-2xl bg-gray-600 text-white "
+            placeholder="Password"
+            required
+          ></input>
+          <div className="flex items-center justify-between max-w-[300px] mx-auto mt-6">
+            <label htmlFor="remember">
+              <input
+                type="checkbox"
+                id="remember"
+                name="remember"
+                className="inline-block mr-6"
+              ></input>
+              Remember Me
+            </label>
+            <button className="text-purple-400">Forgot Password</button>
+          </div>
+          <div className="w-[80%] mx-auto mt-6">
+            <button
+              type="button"
+              className="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
+            >
+              Log In!
+            </button>
+          </div>
+        </form>
+      )}
     </div>
   );
 };
