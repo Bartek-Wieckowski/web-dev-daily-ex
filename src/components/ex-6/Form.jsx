ae80f7896d38291dc7733c6913831f09cdf7eb03
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-11-12 12:40:05 +0100

    coding exercise with useReducer

--- src/components/ex-6/Form.jsx
+++ src/components/ex-6/Form.jsx
@@ -1,43 +1,38 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import FormLogin from './FormLogin';
 import FormSignup from './FormSignup';
+import formReducer, { initialState } from './FormReducer';
 
 const Form = () => {
-  const [activeTabLogIn, setactiveTabLogIn] = useState(true);
-  const [activeTabSignUp, setactiveTabSignUp] = useState(false);
-  const handleLogIn = () => {
-    setactiveTabSignUp(false);
-    setactiveTabLogIn(true);
-  };
-  const handleSignUp = () => {
-    setactiveTabLogIn(false);
-    setactiveTabSignUp(true);
-  };
+  const [{ loginTab, signupTab }, dispatch] = useReducer(
+    formReducer,
+    initialState
+  );
 
   return (
     <div className="relative max-w-[500px] pt-10 pb-20 mx-auto bg-slate-700 text-white rounded-3xl">
       <div className="absolute -top-7 left-1/2 w-14 h-14 bg-teal-400 "></div>
       <div className="flex items-center justify-between max-w-[300px] py-10 mx-auto">
         <button
-          onClick={() => handleLogIn()}
+          onClick={() => dispatch({ type: 'loginTab/actived' })}
           className={`border-b-2  ${
-            activeTabLogIn ? 'border-gray-100' : 'border-gray-500'
+            loginTab ? 'border-gray-100' : 'border-gray-500'
           }`}
         >
           Log in
         </button>
         <button
-          onClick={() => handleSignUp()}
+          onClick={() => dispatch({ type: 'signupTab/actived' })}
           className={`border-b-2  ${
-            activeTabSignUp ? 'border-gray-100' : 'border-gray-500'
+            signupTab ? 'border-gray-100' : 'border-gray-500'
           }`}
         >
           Sign up
         </button>
       </div>
 
-      {activeTabLogIn && <FormLogin />}
-      {activeTabSignUp && <FormSignup />}
+      {loginTab && <FormLogin />}
+      {signupTab && <FormSignup />}
     </div>
   );
 };
