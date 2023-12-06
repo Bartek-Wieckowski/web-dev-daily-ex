4dede645b10442ac5f41efbee1001f476ab131bf
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-12-06 20:24:25 +0100

    created design effect

--- src/components/ex-8/Exercise8.jsx
+++ src/components/ex-8/Exercise8.jsx
@@ -1,5 +1,9 @@
 const Exercise8 = () => {
-  return <div>test</div>;
+  return (
+    <section className="bg-stone-100 text-black flex flex-col justify-center items-center mx-auto no_scroll">
+      <div className=" bg-blue-300  max-w-[400px] w-full h-[400px] rounded-full"></div>
+    </section>
+  );
 };
 
 export default Exercise8;
