d78f8d7ce846432a0530647ab958513c69fcb5b9
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-12-07 20:46:10 +0100

    remove test and back to baisc hover colour

--- src/components/ex-8/Exercise8.jsx
+++ src/components/ex-8/Exercise8.jsx
@@ -3,8 +3,8 @@ const Exercise8 = () => {
     <section className="bg-stone-100 text-black flex flex-col justify-center items-center mx-auto no_scroll">
       <div className="max-w-[500px] w-full mx-auto relative">
         <div className=" bg-blue-300 max-w-[400px] w-full h-[400px] mx-auto rounded-full"></div>
-        <button className="absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] bg-indigo-800 text-white rounded-xl  px-6 py-4 hover:bg-indigo-900 transition-colors">
-          Share open1
+        <button className="absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] bg-indigo-800 text-white rounded-xl  px-6 py-4 hover:bg-indigo-950 transition-colors">
+          Share open
         </button>
       </div>
     </section>
