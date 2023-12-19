64f4ee2a4860af72159e88022893c7d9ac9e2aaf
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-12-19 11:50:18 +0100

    test prettier tailwind order class

--- src/components/ex-8/Exercise8.jsx
+++ src/components/ex-8/Exercise8.jsx
@@ -1,20 +1,20 @@
-import { useState } from 'react';
-import { createPortal } from 'react-dom';
-import { BiShareAlt } from 'react-icons/bi';
-import { ToastContainer } from 'react-toastify';
+import { useState } from "react";
+import { createPortal } from "react-dom";
+import { BiShareAlt } from "react-icons/bi";
+import { ToastContainer } from "react-toastify";
 
-import ShareModal from './ShareModal';
+import ShareModal from "./ShareModal";
 
 const Exercise8 = () => {
   const [modalOpen, setModalOpen] = useState(false);
   return (
     <>
       <ToastContainer />
-      <section className="bg-stone-100 text-black flex flex-col justify-center items-center mx-auto no_scroll">
-        <div className="max-w-[500px] w-full mx-auto relative">
-          <div className="bg-blue-300 max-w-[400px] w-full h-[400px] mx-auto rounded-full"></div>
+      <section className="no_scroll mx-auto flex flex-col items-center justify-center bg-stone-100 text-black">
+        <div className="relative mx-auto w-full max-w-[500px]">
+          <div className="mx-auto h-[400px] w-full max-w-[400px] rounded-full bg-blue-300"></div>
           <button
-            className="absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] bg-indigo-800 text-white rounded-xl px-6 py-4 flex items-center justify-center gap-3 hover:bg-indigo-950 transition-colors"
+            className="absolute left-1/2 top-1/2 flex translate-x-[-50%] translate-y-[-50%] items-center justify-center gap-3 rounded-xl bg-indigo-800 px-6 py-4 text-white transition-colors hover:bg-indigo-950"
             onClick={() => setModalOpen(true)}
           >
             <span>
@@ -26,7 +26,7 @@ const Exercise8 = () => {
         {modalOpen &&
           createPortal(
             <ShareModal onClose={() => setModalOpen(false)} />,
-            document.body
+            document.body,
           )}
       </section>
     </>
