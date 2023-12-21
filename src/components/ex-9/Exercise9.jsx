45c7ac6acc920b3ee773f831e6a923a717fac6a7
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-12-21 09:48:28 +0100

    add function to close first modal window

--- src/components/ex-9/Exercise9.jsx
+++ src/components/ex-9/Exercise9.jsx
@@ -6,12 +6,14 @@ import ThanksModal from "./ThanksModal";
 
 function Exercise9() {
   const [selectedButton, setSelectedButton] = useState(null);
+  const [showWindow, setShowWindow] = useState(false);
 
   const buttons = [
     { id: 1, emoji: "😥", label: "Unhappy" },
     { id: 2, emoji: "😑", label: "Neutral" },
     { id: 3, emoji: "😁", label: "Satisfied" },
   ];
+  const hiddenClass = "hidden";
 
   function handleButtonClick(buttonId) {
     if (selectedButton === null) {
@@ -19,12 +21,23 @@ function Exercise9() {
     }
   }
 
+  function handleCloseWindow() {
+    setShowWindow(true);
+  }
+
   return (
     <section className="relative grid h-screen place-items-center bg-stone-100">
-      <div className="z-0 w-full max-w-[500px] rounded-lg bg-white shadow-lg">
+      <div
+        className={`z-0 w-full max-w-[500px] rounded-lg bg-white shadow-lg ${
+          showWindow ? hiddenClass : ""
+        }`}
+      >
         <div className="flex h-full flex-col">
           <div className="ml-auto p-4">
-            <HiMiniXMark className="cursor-pointer text-3xl font-bold text-gray-500" />
+            <HiMiniXMark
+              className="cursor-pointer text-3xl font-bold text-gray-500"
+              onClick={handleCloseWindow}
+            />
           </div>
           <p className="mx-auto mb-6 w-full max-w-[350px] text-center font-bold text-slate-800">
             How satisfied are you with webdevdaily`s customer support
@@ -46,7 +59,10 @@ function Exercise9() {
             ))}
           </div>
           <div className="mx-auto my-6 w-full max-w-[150px]">
-            <button className="block w-full rounded-md bg-slate-900 px-6 py-3 text-center capitalize text-white transition-colors hover:bg-slate-600">
+            <button
+              className="block w-full rounded-md bg-slate-900 px-6 py-3 text-center capitalize text-white transition-colors hover:bg-slate-600"
+              onClick={handleCloseWindow}
+            >
               Close
             </button>
           </div>
